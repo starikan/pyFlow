232ad9bf9cdeb18533b464a2c0fc034489d144c5
Starik <4analit@gmail.com>
2017-10-13 08:30:34 +0400

    Extract initial test data into the file

--- vue/src/vuex/store.js
+++ vue/src/vuex/store.js
@@ -1,58 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import initData from "./init_data";
+
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   state: {
-    count: 0,
     currFlowId: "test_flow",
     selectedBlock: null,
     activeBlock: null,
-    flows: {
-      test_flow: {
-        operators: {
-          operator1: {
-            top: 20,
-            left: 20,
-            properties: {
-              title: "Operator 1",
-              inputs: {},
-              outputs: {
-                output_1: {
-                  label: "Output 1"
-                }
-              }
-            }
-          },
-          operator2: {
-            top: 80,
-            left: 300,
-            properties: {
-              title: "Operator 2",
-              inputs: {
-                input_1: {
-                  label: "Input 1"
-                },
-                input_2: {
-                  label: "Input 2"
-                }
-              },
-              outputs: {}
-            }
-          }
-        },
-        links: {
-          link_1: {
-            fromOperator: "operator1",
-            fromConnector: "output_1",
-            toOperator: "operator2",
-            toConnector: "input_2"
-          }
-        }
-      }
-    }
+    flows: initData
   },
   mutations: {
     setBlockPosition(state, { blockId, position }) {
