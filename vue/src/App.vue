aa445c0ac6c4d3675052468f8b56e97653edcdab
Starik <4analit@gmail.com>
2017-10-13 08:35:28 +0400

    Add Left panel

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
+
     <flow></flow>
-    <!-- <left-panel></left-panel>
-      <info-panel></info-panel>
-      <zoom-image></zoom-image> -->
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <hello></hello> -->
+    <left-panel></left-panel>
+    <!-- <info-panel></info-panel> -->
+    <!-- <zoom-image></zoom-image> -->
+
     <!-- <router-view></router-view> -->
   </div>
 </template>
@@ -13,10 +13,11 @@
 <script>
 
 import Flow from "./components/Flow";
+import LeftPanel from "./components/LeftPanel";
 
 export default {
   name: 'app',
-  components: { Flow }
+  components: { Flow, LeftPanel }
 }
 </script>
 
---
+++ vue/src/components/LeftPanel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+    </div>
+</template>
+
+
+
+<script>
+
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+    name: 'left-panel',
+    computed: {
+        // ...mapState(['count',]),
+        // ...mapGetters(['countPlus10',])
+    },
+    methods: {
+        // ...mapMutations(['increment']),
+        // ...mapActions({ incrementAsync: 'increment' })
+    }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
